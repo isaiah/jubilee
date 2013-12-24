0cafa0d33df752e18f018a73326a85491849f6f0
Isaiah Peng <isaiah.peng@vcint.com>
2013-12-24 15:42:26 +0100

    update chatapp to demonstrate the full strength of vertx event bus

--- examples/chatapp/app.rb
+++ examples/chatapp/app.rb
@@ -1,5 +1,25 @@
 require 'bundler/setup'
 Bundler.require(:default)
+require 'vertx'
+
+def to_a(shared_set)
+  ret = []
+  shared_set.each{ |item| ret << item}
+  ret
+end
+
+Vertx::EventBus.register_handler('logout') do |message|
+  Vertx::SharedData.get_set(:users).delete(message.body)
+end
+
+# register the user and return the pervious users
+Vertx::EventBus.register_handler('login') do |message|
+  user = message.body
+  users = Vertx::SharedData.get_set(:users).add(user)
+  message.reply(users: to_a(users))
+  Vertx::EventBus.publish("new_user", user)
+end
+
 
 get "/" do
   haml :index
@@ -24,6 +44,10 @@ __END__
 @@index
 #updates
 
+%p
+  %label receiver
+  %select#receivers(name="receiver")
+    %option all
 %p
   %textarea#content(name="content" rows=8 cols=80)
 
--- examples/chatapp/public/assets/javascripts/application.js
+++ examples/chatapp/public/assets/javascripts/application.js
@@ -1,14 +1,67 @@
+// To generate a random id 
+function s4() {
+  return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+};
+
 $(function() {
   var eb = new vertx.EventBus("/eventbus");
+  var updates = $("#updates");
+  var uid = "Guest_" + s4();
 
   eb.onopen = function() {
-                eb.registerHandler("chat", function(data) {
-                  $("#updates").append(data + "<br/>");
-                });
-              }
+    eb.send("login", uid, function(data){
+      for(var i = 0; i < data.users.length; i++) {
+        if (data.users[i] != uid) $("#receivers").append("<option>" + data.users[i] + "</option>");
+      }
+    });
+    updates.html("<h5>Welcome to the Jubilee chat room!</h5>");
+    eb.registerHandler("chat", function(data) {
+      if (data.sender != uid)
+        updates.append("<div class='public'><span class='sender'>" + data.sender + " said:</span>" + data.message + "</div>");
+      else
+        updates.append("<div class='public by_you'><span class='sender'>You said:</span>" + data.message + "</div>");
+    });
 
-  $("#send").click(function() {
-    eb.publish("chat", $("#content").val());
+    eb.registerHandler("new_user", function(data) {
+      if (data != uid) {
+        $("#receivers").append("<option>" + data + "</option");
+        updates.append("<div class='login'>" + data + " joined the room.</div>");
+      }
+    });
+
+    eb.registerHandler(uid, function(data) {
+      updates.append("<div class='private'><span class='sender'>" + data.sender + " said to you:</span>" + data.message + "</div>");
+    });
+
+    eb.registerHandler("logout", function(data) {
+      $('#receivers option:contains("' + data + '")').remove();
+      updates.append("<div class='logout'>" + data + " left the room.</div>");
+    });
+
+    window.onbeforeunload = function() {
+      eb.publish("logout", uid);
+    }
+  }
+
+  var sendMessage = function() {
+    var msg = $("#content").val();
+    if ((receiver = $("#receivers").val()) === "all") {
+      eb.publish("chat", {sender: uid, message: msg});
+    } else {
+      updates.append("<div class='public by_you'><span class='sender'>You said to " + uid + ":</span>" + msg + "</div>");
+      eb.send(receiver, {sender: uid, message: msg});
+    }
     $("#content").val("");
+  }
+
+  $("#send").click(sendMessage);
+
+  $(document.body).keyup(function(ev) {
+    if (ev.which === 13) {
+      sendMessage();
+    }
   });
+
 });
